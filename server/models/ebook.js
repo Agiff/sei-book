51452ed46c151131a28e8cd78b51e137e63b3584
Mohammad Rahadian Ghifari <ghifarimohammadrahadian@gmail.com>
2023-04-28 04:54:54 +0700

    feat: add Ebook model and table

--- server/models/ebook.js
+++ server/models/ebook.js
@@ -3,30 +3,31 @@ const {
   Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class User extends Model {
+  class Ebook extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      User.hasMany(models.Ebook);
+      Ebook.belongsTo(models.User);
     }
   }
-  User.init({
+  Ebook.init({
     id: {
       allowNull: false,
       autoIncrement: true,
       primaryKey: true,
       type: DataTypes.INTEGER
     },
-    email: DataTypes.STRING,
-    password: DataTypes.STRING,
-    name: DataTypes.STRING,
-    role: DataTypes.STRING
+    originalName: DataTypes.STRING,
+    fileName: DataTypes.STRING,
+    path: DataTypes.STRING,
+    size: DataTypes.INTEGER,
+    UserId: DataTypes.INTEGER
   }, {
     sequelize,
-    modelName: 'User',
+    modelName: 'Ebook',
   });
-  return User;
+  return Ebook;
 };
